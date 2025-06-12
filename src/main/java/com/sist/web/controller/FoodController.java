a39ea145a8e35341d2cd318ec71c9a0c2a3f6fa3
KwonHJ7517 <gurwns7517@naver.com>
2025-06-12 14:42:18 +0900

    1111

--- src/main/java/com/sist/web/controller/FoodController.java
+++ src/main/java/com/sist/web/controller/FoodController.java
@@ -11,45 +11,52 @@ import java.util.*;
 import com.sist.web.entity.FoodEntity;
 import com.sist.web.service.*;
 import com.sist.web.vo.FoodListVO;
-
 @Controller
+/*
+ *   1. Git Action 
+ *   2. 새로운 운영체제 : Docker 
+ *                      |
+ *                   쿠바네티스 => 클러스트 
+ *   ------------------------  젠킨스 
+ */
 public class FoodController {
-	@Autowired
-	private FoodService fService;
-	
-	@GetMapping("/")
-	public String food_main(@RequestParam(name="page",required = false)String page,Model model)
-	{
-		if(page==null)
-			page="1";
-		int curpage=Integer.parseInt(page);
-		int rowSize=12;
-		int start=(rowSize*curpage)-(rowSize-1);
-		int end=rowSize*curpage;
-		List<FoodListVO> list=fService.foodListData(start, end);
-		int totalpage=fService.foodTotalpage();
-		final int BLOCK=10;
-		int startPage=((curpage-1)/BLOCK*BLOCK)+1;
-		int endPage=((curpage-1)/BLOCK*BLOCK)+BLOCK;
-		
-		if(endPage>totalpage)
-			endPage=totalpage;
-		
-		model.addAttribute("list",list);
-		model.addAttribute("curpage",curpage);
-		model.addAttribute("totalpage",totalpage);
-		model.addAttribute("startPage",startPage);
-		model.addAttribute("endPage",endPage);
-		model.addAttribute("main_html", "main/home");
-		return "";
-	}
-	
-	@GetMapping("/detail")
-	public String food_detail(@RequestParam("fno")int fno,Model model)
-	{
-		FoodEntity vo=fService.FoodDetailData(fno);
-		model.addAttribute("vo",vo);
-		model.addAttribute("main_html", "food/detail");
-		return "detail";
-	}
+   @Autowired
+   private FoodService fService;
+   
+   @GetMapping("/") // 파일명 / redirect => 파일에 출력할 데이터 , 파일명 전송 
+   public String food_main(@RequestParam(name="page",required = false) String page,Model model)
+   {
+	   if(page==null)
+		   page="1";
+	   int curpage=Integer.parseInt(page);
+	   int rowSize=12;
+	   int start=(rowSize*curpage)-(rowSize-1);
+	   int end=rowSize*curpage;
+	   List<FoodListVO> list=fService.foodListData(start, end);
+	   int totalpage=fService.foodTotalpage();
+	   
+	   final int BLOCK=10;
+	   int startPage=((curpage-1)/BLOCK*BLOCK)+1;
+	   int endPage=((curpage-1)/BLOCK*BLOCK)+BLOCK;
+	   
+	   if(endPage>totalpage)
+		   endPage=totalpage;
+	   
+	   // 데이터를 전송 
+	   model.addAttribute("list", list);
+	   model.addAttribute("curpage", curpage);
+	   model.addAttribute("totalpage", totalpage);
+	   model.addAttribute("startPage", startPage);
+	   model.addAttribute("endPage", endPage);
+	   model.addAttribute("main_html", "main/home");
+	   return "index";
+   }
+   @GetMapping("/detail")
+   public String food_detail(@RequestParam("fno") int fno,Model model)
+   {
+	   FoodEntity vo=fService.FoodDetailData(fno);
+	   model.addAttribute("vo", vo);
+	   model.addAttribute("main_html", "food/detail");
+	   return "index";
+   }
 }
